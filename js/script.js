81d70560fb76e9274d23e1180b042c6145b8db5b
xelilov <xelilov-x@mail.ru>
2017-12-12 11:35:32 +0400

    ders1

--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
+$(document.body).append("<div class='bgSlider'></div");
+$(".bgSlider").append("<div class='firstRow'></div");
+$(".bgSlider").append("<div class='secondRow'></div");
+
+
+for(var i=0;i<8;i++){
+	$(".firstRow").append("<div></div>")
+	$(".secondRow").append("<div></div>")
+}
+
+var firstImages=["img01.jpg","img02.jpg","img03.jpg","img04.jpg","img05.jpg","img06.jpg","img07.jpg","img08.jpg",]
+for(var i=0;i<firstImages.length;i++){
+	$(".firstRow").children("div").eq(i).append("<img src='images/"+firstImages[i]+"'>")
+}
+var secondImages=["img09.jpg","img10.jpg","img11.jpg","img12.jpg","img13.jpg","img14.jpg","img15.jpg","img16.jpg",]
+for(var i=0;i<secondImages.length;i++){
+	$(".secondRow").children("div").eq(i).append("<img src='images/"+secondImages[i]+"'>")
+}
+
+$(".bgSlider").append("<span class='up'>/\\</span>")
+$(".bgSlider").append("<span class='down'>\\/</span>")
+
+
+
 $(".bgSlider").find("div").find("div").eq(0).css({"top":"0","left":"0"});
 $(".bgSlider").find("div").find("div").eq(1).css({"top":"0","left":"205px"});
 $(".bgSlider").find("div").find("div").eq(2).css({"top":"0","left":"410px"});
